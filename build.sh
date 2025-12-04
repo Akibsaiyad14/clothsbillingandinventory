099f391c0fe2f197f050001f2a71e8efed29c920
Akibsaiyad14 <akibsaiyad163@gmail.com>
2025-12-04 14:20:10 +0530

    updated the req.txt

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 # exit on error
 set -o errexit
 
-cd backend
+# Install dependencies from backend directory
+pip install -r backend/requirements.txt
 
-# Install dependencies
-pip install -r requirements.txt
+# Change to backend directory
+cd backend
 
 # Collect static files
 python manage.py collectstatic --no-input
